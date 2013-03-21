41cfecbac9d19a73d4e82b0546543a28464c4283
Colin Curtin <colin.t.curtin@gmail.com>
2013-03-21 07:26:58 -0700

    First bootstrap: rails new . -f

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ComputerBuilder::Application.config.secret_key_base = '0256a246f7af2be12d2da66436cd116fd7ce6f8bfd43c241ea07b563e7321b95b174134b4783a57f6386f19f349b9d28834ffc72887d98d7f9caa6c6265557be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ComputerBuilder::Application.config.session_store :encrypted_cookie_store, key: '_computer-builder_session'
